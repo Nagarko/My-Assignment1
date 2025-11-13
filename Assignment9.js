803b90be5fe3fb1d778a44bed9ab0cb39edb0ffd
Nagarko <ahmaddrabiu194@gmail.com>
2025-11-13 18:43:58 +0100

    Create Assignment9.js

---
+++ Assignment9.js
@@ -0,0 +1,21 @@
+let i = 1;
+
+while (i <= 20) {
+    console.log(i);
+    i++;
+}
+
+// for loop
+
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// for loop
+let number = 5;
+
+for (let i = 1; i <= 12; i++) {
+    console.log(number + " x " + i + " = " + (number * i));
+}
